742be7f87f9e380d2f1d672a8884d63074353241
Paul Maunders <paul@fubra.com>
2012-08-27 17:23:34 +0100

    Fixed a few typos

--- scripts/create-gold-master.sh
+++ scripts/create-gold-master.sh
@@ -14,12 +14,12 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # Remove hostname references...
     sed -i '/HOSTNAME/d' /etc/sysconfig/network
-    sed -i '/HOSTNAME/d' etc/sysconfig/network-scripts/ifcfg-eth0
+    sed -i '/HOSTNAME/d' /etc/sysconfig/network-scripts/ifcfg-eth0
     rm /etc/hostname
     yum clean all
     # Remove all mac address references
     rm /etc/udev/rules.d/70-persistent-net.rules
-    sed -i '/HWADDR/d' etc/sysconfig/network-scripts/ifcfg-eth0
+    sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
     # Remove the dhcp lease history
     rm -f /var/lib/dhclient/dhclient-eth0.leases
     # Remove the SSH Host keys etc...
